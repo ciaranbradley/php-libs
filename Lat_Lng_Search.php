a502f28556b5fed653e8cbc7eb830f5064362078
Ciaran Bradley <ciaran.p.bradley@gmail.com>
2010-11-04 15:20:02 +0000

    Tidy up Lat_Lng

--- Lat_Lng_Search.php
+++ Lat_Lng_Search.php
@@ -1,25 +1,32 @@
 <?php 
-/***
- * 
- * 	Quick/Dirty abstraction of the google lat long search
+/**
+ * Library class Lat_Lng_Search
  *  
+ * Leverages Googlemap API to pull back a (WGS84?) lat long pair.  Requires CURL 
  * 
+ * @author Ciaran Bradley
+ * 
+ * @usage $lat_lng_array = Lat_Lng_Search::lookup_address("1 The North Pole, North Pole");
  */
-
 class Lat_Lng_Search
 {
-	
+	/**
+	 * Performs the search using google api
+	 * 
+	 * @param $address string
+	 * @return $result or false (result is array('lat' => float, 'lng' => float) 
+	 */
 	public static function lookup_address($address)
 	{
 		
 		$url = 'http://maps.google.com/maps/api/geocode/xml?address=' . urlencode($address) . '&sensor=true';
-		//echo $url;
 		
 		if(in_array('curl', get_loaded_extensions()))
 		{
 			$curl = curl_init($url);
 			
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			
 			$response = curl_exec($curl);
 			
 			curl_close($curl);
@@ -38,19 +45,14 @@ class Lat_Lng_Search
 			fclose($handle);
 		}
 		
-		//print_array($response);
-		//exit();
-		
 		$xml = new SimpleXMLElement($response);
-		
-		//print_array($xml);
-		//exit();
-		
+
 		$status = exist($xml->status);
 		
 		if($status == 'OK')
 		{
 			$lat = (string) $xml->result->geometry->location->lat;
+			
 			$lng = (string) $xml->result->geometry->location->lng;
 			
 			if($lat && $lng && trim($lat) != "" && trim($lng) != "")
@@ -62,16 +64,12 @@ class Lat_Lng_Search
 				
 				);
 				
-				//print_array($result); 
-				//exit();
-				
 				return $result;
 			}
 		}
 		
-		return false;
+		return false; // If you get here, it's all gone wrong
 	}
 	
-}
-
+}// End Lat_Lng_Search
 ?>
